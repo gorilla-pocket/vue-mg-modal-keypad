68f72308fa9ab8e9858c4b3f97b0e49cbf38e965
yamakawa <yamakawa@mountain-gorilla.co.jp>
2020-03-25 12:58:09 +0900

    幅にボタンが収まらない場合があるため、幅変更できるように

--- example/App.vue
+++ example/App.vue
@@ -2,7 +2,7 @@
   <section class="container mt-2">
     <modal-keypad v-model="value" />
     <modal-keypad v-model="value2" layouts="123|456|789|0-.|{del:backspace}{ok:ok}" />
-    <modal-keypad inputClass="text-danger" v-model="value2" layouts="123|456|789|0-.|{del:backspace}{ok:ok}" />
+    <modal-keypad inputClass="text-danger" v-model="value2" width="350px" layouts="123|456|789|0-.|{del:backspace}{ok:ok}" />
   </section>
 </template>
 
--- src/vue-mg-modal-keypad.vue
+++ src/vue-mg-modal-keypad.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <input type="input" class="form-control modal-keypad" :class="inputClass" readonly v-model="val" @click="show">
-    <modal-dialog :show="showModal" @close="showModal=false" hide-header hide-footer size="custom" width="300px">
+    <modal-dialog :show="showModal" @close="showModal=false" hide-header hide-footer size="custom" :width="width">
         <template v-slot:body>
             <div class="keypad-container">
                 <input type="input" class="form-control form-control-lg display" readonly v-model="val">
@@ -28,6 +28,9 @@ export default {
         },
         inputClass: {
             default: '',
+        },
+        width: {
+            default: '300px',
         }
     },
     data () {
